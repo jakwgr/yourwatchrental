7671d0577502e2bc5abf9651f18e685aead210a9
jakwgr <jwaligora006@gmail.com>
2026-06-30 23:05:12 +0200

    first commit

---
+++ src/main/java/com/yourwatchrental/watchrental/WatchrentalApplication.java
@@ -0,0 +1,13 @@
+package com.yourwatchrental.watchrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchrentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourwatchrental/watchrental/WatchrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourwatchrental.watchrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
